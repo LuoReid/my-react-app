721c3fb783a3a09eddb61685c3ce385f7e978f3d
Luo.Reid <luo.reid@gmail.com>
2018-06-01 22:22:39 +0800

    组合和继承 Demo

--- src/demo.js
+++ src/demo.js
@@ -396,4 +396,58 @@ class Calculator1 extends React.Component {
     )
   }
 }
-ReactDOM.render(<Calculator />,document.querySelector('#foot'))
+function FancyBorder(props){
+  return (
+    <div className={'FancyBorder FancyBorder-'+props.color}>{props.children}</div>
+  )
+}
+function Dialog(props) {
+  return (
+    <FancyBorder color="blue">
+      <h1 className="Dialog-title">{props.title}</h1>
+      <p className="Dialog-message">{props.message}</p>
+      {props.children}
+    </FancyBorder>
+  )
+}
+function WelcomeDialog(){
+  return (
+    <Dialog title="Welcome" message="Thank you for visiting our spacecraft!" />
+  )
+}
+function SplitPane(props) {
+  return (
+    <div className="SplitPane">
+      <div className="SplitPane-left">{props.left}</div>
+      <div className="SplitPane-right">{props.right}</div>
+    </div>
+  )
+}
+class SignUpDialog extends React.Component {
+  constructor(props){
+    super(props)
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSignUp = this.handleSignUp.bind(this)
+    this.state = {login:''}
+  }
+  handleChange(e) {
+    this.setState({login: e.target.value})
+  }
+  handleSignUp() {
+    alert(`Welcome aboard, ${this.state.login}!`)
+  }
+  render() {
+    return (
+      <Dialog title="Mars Exploration Program" message="How should we refer to you?">
+        <input type="text" value={this.state.login} onChange={this.handleChange}/>
+        <button onClick={this.handleSignUp}>Sign Me Up!</button>
+      </Dialog>
+    )
+  }
+}
+/*function App(){
+  return (
+    <SplitPane left={<Contacts />} right={<Chat />} />
+  )
+}*/
+ReactDOM.render(<SignUpDialog />,document.querySelector('#foot'))
